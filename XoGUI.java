5b12135c62e203f7fdc544a927743095bd03a66e
oleynikov-ap <oleynikov-ap@yandex.ru>
2020-08-18 10:52:25 +0300

    Initial commit

---
+++ XoGUI.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.java_one.lesson_g.online;
+
+import javax.swing.*;
+import java.awt.*;
+//JavaFX
+//thymeleaf
+//VAADIN
+//...
+
+public class XoGUI {
+    public static void main(String[] args) {
+        new GameWindow(1);
+    }
+}
